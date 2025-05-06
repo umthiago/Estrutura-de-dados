99f31d9802e6c074ead3a511528b52283d4da897
Thiago <124899004+umthiago@users.noreply.github.com>
2025-05-06 15:03:52 -0300

    Update Ex1.cpp

--- Aula06.05-Classes/Exe_classes.pdf/Ex1.cpp
+++ Aula06.05-Classes/Exe_classes.pdf/Ex1.cpp
@@ -1,1 +1,48 @@
-a
+#include <iostream>
+using namespace std;
+
+class Carro {
+private:
+  string marca;
+  string modelo;
+  int ano;
+  int velocidade;
+
+public:
+  Carro(string m, string mo, int a, int v) {
+    marca = m;
+    modelo = mo;
+    ano = a;
+    velocidade = v;
+  }
+
+  void acelerar() {
+    velocidade += 10;
+    cout << "Acelerando... Velocidade atual: " << velocidade << " km/h" << endl;
+  }
+  void frear() {
+    if (velocidade > 0) {
+      velocidade -= 10;
+      cout << "Freando... Velocidade atual: " << velocidade << " km/h" << endl;
+    } else {
+      cout << "O carro já está parado!" << endl;
+    }
+  }
+  void exibirdadosCarro() {
+    cout << "\nMarca: " << marca << "\nModelo: " << modelo << "\nAno: " << ano
+         << "\nVelocidade: " << velocidade << " km/h" << endl;
+  }
+};
+
+int main(){
+  Carro carro("BMW", "320I", 2012, 0);
+  carro.acelerar();
+  carro.frear();
+  carro.frear();
+  carro.acelerar();
+  carro.acelerar();
+
+  carro.exibirdadosCarro();
+
+  return 0;
+}
